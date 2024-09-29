8bab6e786d408125692c24fa0954333ee2fd13dc
prajapatibhaskar <141663471+prajapatibhaskar@users.noreply.github.com>
2024-09-29 19:46:30 +0530

    Added files and folders.

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import pickle
+from tensorflow.keras.models import load_model
+
+app = Flask(__name__)
+
+# Load the model and tokenizer
+with open('resources/tfidf_tokenizer.pkl', 'rb') as f:
+    tfidf_tokenizer = pickle.load(f)
+loaded_model = load_model('resources/text_classification_model.h5')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    text = data.get('text')
+    
+    if not text or not text.strip():
+        return jsonify({'error': 'No text provided'}), 400
+    
+    text_features = tfidf_tokenizer.transform([text])
+    predictions = loaded_model.predict(text_features)
+    predicted_label = int(predictions[0][0])
+    
+    return jsonify({'predicted_label': predicted_label})
